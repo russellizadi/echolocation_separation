861bf3ae58b98d318f2ab0aada92032441b3f134
russell <mizadi@nd.edu>
2021-06-29 12:26:56 -0400

    update sources

--- sources.py
+++ sources.py
@@ -4,6 +4,8 @@ import utils as ut
 
 # path setting
 lst_args = sys.argv[1:]
+if len(lst_args) == 0:
+    lst_args = ["settings/sources.ini"]
 assert len(lst_args)==1, "Not a valid argument"
 _, name_suffix = lst_args[0].split('.')
 assert name_suffix == 'ini', "Not a valid config"
